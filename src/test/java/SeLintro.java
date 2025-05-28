6cd45a3d29bf9c189530ec811e00f55fcc0d8e90
vineete2 <vineete2@gmail.com>
2025-05-28 03:08:39 +0200

    Initial commit

---
+++ src/test/java/SeLintro.java
@@ -0,0 +1,13 @@
+package src;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeLintro  {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Selenium Introduction!");
+
+        WebDriver driver= new ChromeDriver();
+
+    }
+}
